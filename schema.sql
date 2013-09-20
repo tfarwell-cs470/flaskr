2dd54ca7ceb87e1d18953be94a7523b2750549dc
farwelltj <farwlltj1988@gmail.com>
2013-09-20 10:42:19 -0400

    new table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,19 @@
-drop table if exists entries;
-create table entries (
+
+create table entries if not exists entries ()
   id integer primary key autoincrement,
   title text not null,
   text text not null
 );
+
+create table if not exists users (
+	Username text primary key,
+	password text,
+	email text not null,
+	first_name text,
+	last_name text,
+	role integer
+	dob date
+ );
+
+insert or ignore into users (username, password, email, first_name, last_name)
+	values ("admin", "password", "site","email@site.com", "Siteartor")
